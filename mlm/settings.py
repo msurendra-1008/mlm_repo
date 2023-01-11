81ddb2574b88815b8a07bb60cb001156f07d59e1
msurendra-1008 <111314327+msurendra-1008@users.noreply.github.com>
2023-01-11 16:00:37 +0530

    Update settings.py

--- mlm/settings.py
+++ mlm/settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = 'mlm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+#DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
     
-    'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'mlm',
-       'USER': 'postgres',
-       'PASSWORD': 'admin',
-       'HOST': 'localhost',
-       'PORT': '5432',
-   }
-}
+ #   'default': {
+  #     'ENGINE': 'django.db.backends.postgresql',
+   #    'NAME': 'mlm',
+    #   'USER': 'postgres',
+     #  'PASSWORD': 'admin',
+      # 'HOST': 'localhost',
+       #'PORT': '5432',
+   #}
+#}
 
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
